51f5c06f89848bbab6828bc80f4907e83e38b57f
xcwan <523067817@qq.com>
2020-09-18 17:26:06 +0800

    Parse, scp, ssh test pass

--- cli/cmd/submit.go
+++ cli/cmd/submit.go
@@ -1,38 +1,17 @@
 package cmd
 
 import (
-	"fmt"
 	"github.com/spf13/cobra"
-	"log"
-	"os/exec"
 	"tcloud-sdk/cli/tcloudcli"
 )
 
+// TODO(), By default, no args
 func NewSubmitCommand(cli *tcloudcli.TcloudCli) *cobra.Command {
 	return &cobra.Command{
 		Use:   "submit",
-		Short: "Submit a job",
+		Short: "Submit a slurm job",
 		Run: func(cmd *cobra.Command, args []string) {
-			// fmt.Println("tcloud submit CLI")
-			// run TACC1:/home/ubuntu/mnist_demo/run.slurm
-			err := SubmitJob()
-			if err {
-				fmt.Println("Fail to submit job.")
-				log.Fatal(err)
-			}
+			cli.XSubmit(args...)
 		},
 	}
-}
-
-func SubmitJob() bool {
-	bash_command := "ssh ubuntu@TACC1 \"sbatch /home/ubuntu/mnist_demo/run.slurm\""
-	cmd := exec.Command("ssh", "-i", "/Users/xcwan/Downloads/TACC.pem", "ubuntu@18.162.45.250", bash_command)
-	fmt.Println(cmd)
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		log.Fatalf("cmd.Run() failed with %s\n", err)
-		return true
-	}
-	fmt.Printf("Job submitted\n", string(out))
-	return false
 }
--- cli/tcloudcli/tcloudcli.go
+++ cli/tcloudcli/tcloudcli.go
@@ -2,32 +2,24 @@ package tcloudcli
 
 import (
 	"fmt"
-	"io"
 	"io/ioutil"
-	// "log"
-	"bytes"
 	"net"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strings"
 
+	shellquote "github.com/gonuts/go-shellquote"
 	"golang.org/x/crypto/ssh"
 )
 
-type SSHTunnel struct {
-	sess *ssh.Session
-	in   *io.Reader
-	out  *io.Writer
-	err  *io.Writer
-}
-
 type TcloudCli struct {
 	userConfig *UserConfig
-	tunnel     *SSHTunnel
-	prefix     string
+	// sess       *ssh.Session
+	prefix string
 }
 
-func (tcloudcli *TcloudCli) NewSession() *SSHTunnel {
-	var tunnel *SSHTunnel
+func (tcloudcli *TcloudCli) NewSession() *ssh.Session {
 	buffer, err := ioutil.ReadFile(tcloudcli.userConfig.authFile)
 	if err != nil {
 		fmt.Println("Failed to read authFile at %s", tcloudcli.userConfig.authFile)
@@ -63,22 +55,7 @@ func (tcloudcli *TcloudCli) NewSession() *SSHTunnel {
 		fmt.Println("Failed to request for pseudo terminal: ", err)
 		return nil
 	}
-	// Print stdout
-	sess_stdin, err := session.StdinPipe()
-	sess_stdout, err := session.StdoutPipe()
-	sess_stderr, err := session.StderrPipe()
-	tunnel.sess = session
-	tunnel.in = &sess_stdin
-	tunnel.out = &sess_stdout
-	tunnel.err = &sess_stderr
-
-	// Start remote shell
-	err = tunnel.sess.Shell()
-	if err != nil {
-		fmt.Println("Failed to start remote shell: ", err)
-		return nil
-	}
-	return tunnel
+	return session
 }
 
 func (tcloudcli *TcloudCli) NewPrefix() {
@@ -96,12 +73,73 @@ func NewTcloudCli(userConfig *UserConfig) *TcloudCli {
 	tcloudcli := &TcloudCli{
 		userConfig: userConfig,
 	}
-	if tcloudcli.tunnel = tcloudcli.NewSession(); tcloudcli.tunnel == nil {
-		fmt.Println("Failed to start remote session")
-		os.Exit(-1)
-	}
 	tcloudcli.NewPrefix()
 	return tcloudcli
+}
+
+func (tcloudcli *TcloudCli) RemoteExecCmd(cmd string) bool {
+	sess := tcloudcli.NewSession()
+	if sess == nil {
+		fmt.Println("Failed to create remote session")
+		os.Exit(-1)
+	}
+	w, err := sess.StdinPipe()
+	if err != nil {
+		fmt.Println("Failed to create StdinPipe", err)
+		return true
+	}
+
+	if err := sess.Run(cmd); err != nil {
+		fmt.Println("Failed to run cmd in SendToCluster ", err)
+		w.Close()
+		return true
+	}
+	defer sess.Close()
+
+	errors := make(chan error)
+	go func() {
+		errors <- sess.Wait()
+	}()
+	fmt.Fprint(w, "\x00")
+	w.Close()
+	return false
+}
+
+func (tcloudcli *TcloudCli) SendToCluster(repoName string, src string) (string, bool) {
+	f, err := os.Stat(src)
+	if err != nil {
+		fmt.Println("Failed to send to cluster. %s not exists.", src)
+		return "", true
+	}
+	prefix := ""
+	if mode := f.Mode(); mode.IsDir() {
+		prefix = "-r"
+	}
+
+	// TODO(A bit wrong when transmit file. Not the same directory as src)
+	dst := tcloudcli.userConfig.SSHpath[0]
+	dst = fmt.Sprintf("%s@%s:/home/%s/", tcloudcli.userConfig.UserName, dst, tcloudcli.userConfig.UserName)
+	cmd := exec.Command("scp", prefix, "-i", tcloudcli.userConfig.authFile, src, dst)
+	if _, err := cmd.CombinedOutput(); err != nil {
+		fmt.Println("Failed to run cmd in SendToCluster ", err)
+		return dst, true
+	}
+	if len(tcloudcli.userConfig.SSHpath) < 2 {
+		return dst, false
+	} else if len(tcloudcli.userConfig.SSHpath) == 2 {
+		src := fmt.Sprintf("/home/%s/%s", tcloudcli.userConfig.UserName, repoName)
+		dst := tcloudcli.userConfig.SSHpath[1]
+		dst = fmt.Sprintf("%s@%s:/home/%s/", tcloudcli.userConfig.UserName, dst, tcloudcli.userConfig.UserName)
+		cmd := shellquote.Join("scp", prefix, src, dst)
+		if err := tcloudcli.RemoteExecCmd(cmd); err == true {
+			fmt.Println("Failed to send repo from Host:", tcloudcli.userConfig.SSHpath[0], " to Host:", tcloudcli.userConfig.SSHpath[1])
+			return dst, true
+		}
+		return dst, false
+	} else {
+		fmt.Println("Not support multi-hop send")
+		return dst, true
+	}
 }
 
 func (tcloudcli *TcloudCli) XBuild(args ...string) {
@@ -111,7 +149,7 @@ func (tcloudcli *TcloudCli) XBuild(args ...string) {
 		fmt.Println("Parse tuxiv config file failed.")
 		os.Exit(-1)
 	}
-	if err = tcloudcli.UploadRepo(localWorkDir); err == true {
+	if err = tcloudcli.UploadRepo(repoName, localWorkDir); err == true {
 		fmt.Println("Upload repository env failed")
 		os.Exit(-1)
 	}
@@ -124,76 +162,8 @@ func (tcloudcli *TcloudCli) XBuild(args ...string) {
 		os.Exit(-1)
 	}
 }
-
-func (tcloudcli *TcloudCli) SendToCluster(src string) (string, bool) {
-	f, err := os.Stat(src)
-	if err != nil {
-		fmt.Println("Failed to send to cluster. %s not exists.", src)
-		return "", true
-	}
-	prefix := ""
-	if mode := f.Mode(); mode.IsDir() {
-		prefix = "-r"
-	}
-
-	// TODO(A bit wrong when transmit file. Not the same directory as src)
-	dst := tcloudcli.userConfig.SSHpath[len(tcloudcli.userConfig.SSHpath)-1]
-	dst = fmt.Sprintf("%s@%s:/home/%s", tcloudcli.userConfig.UserName, dst, tcloudcli.userConfig.UserName)
-	cmd := exec.Command("")
-	if len(tcloudcli.userConfig.SSHpath) < 2 {
-		cmd = exec.Command("scp", "-i", tcloudcli.userConfig.authFile, prefix, src, dst)
-	} else {
-		str := ""
-		sshpath := tcloudcli.userConfig.SSHpath
-		for _, s := range sshpath[:len(sshpath)-1] {
-			str = str + fmt.Sprintf(`ssh -i %s -o StrictHostKeyChecking=no -A -t %s@%s `, tcloudcli.userConfig.authFile, tcloudcli.userConfig.UserName, s)
-		}
-		// str = str + "-W %h:%p"
-		proxycmd := fmt.Sprintf(`ProxyCommand="%s"`, str)
-		cmd = exec.Command("scp", "-i", tcloudcli.userConfig.authFile, "-o", proxycmd, ` -o StrictHostKeyChecking=no`, prefix, src, dst)
-	}
-	fmt.Println(cmd)
-	var out bytes.Buffer
-	var stderr bytes.Buffer
-	cmd.Stdout = &out
-	cmd.Stderr = &stderr
-	if err := cmd.Run(); err != nil {
-		// if exitError, ok := err.(*exec.ExitError); ok {
-		// 	fmt.Println("Failed to run cmd in SendToCluster ", exitError.ExitCode())
-		// 	return dst, true
-		// }
-		fmt.Println(fmt.Sprint(err) + ": " + stderr.String())
-		fmt.Println("Failed to run cmd in SendToCluster ", err)
-		return dst, true
-	}
-	return dst, false
-}
-
-func (tcloudcli *TcloudCli) UploadRepo(localWorkDir string) bool {
-	// cmd := exec.Command("scp", "-r", "-i", tcloudcli.userConfig.authFile, "../tcloud_job", "ubuntu@18.162.45.250:/home/ubuntu")
-	// out, err := cmd.CombinedOutput()
-	// if err != nil {
-	// 	log.Fatalf("cmd.Run() failed with %s\n", err)
-	// 	return true
-	// }
-	// fmt.Printf("Upload file to TACC JUMP out:\n%s\n", string(out))
-	// bash_command := "scp -r /home/ubuntu/tcloud_job TACC1:/home/ubuntu"
-	// cmd = exec.Command("ssh", "-i", tcloudcli.userConfig.authFile, "ubuntu@18.162.45.250", bash_command)
-	// out, err = cmd.CombinedOutput()
-	// if err != nil {
-	// 	log.Fatalf("cmd.Run() failed with %s\n", err)
-	// 	return true
-	// }
-	// fmt.Printf("Upload file to TACC1 out:\n%s\n", string(out))
-	// bash_command = "scp -r /home/ubuntu/tcloud_job TACC2:/home/ubuntu"
-	// cmd = exec.Command("ssh", "-i", tcloudcli.userConfig.authFile, "ubuntu@18.162.45.250", bash_command)
-	// out, err = cmd.CombinedOutput()
-	// if err != nil {
-	// 	log.Fatalf("cmd.Run() failed with %s\n", err)
-	// 	return true
-	// }
-	// fmt.Printf("Upload file to TACC2 out:\n%s\n", string(out))
-	dst, err := tcloudcli.SendToCluster(localWorkDir)
+func (tcloudcli *TcloudCli) UploadRepo(repoName string, localWorkDir string) bool {
+	dst, err := tcloudcli.SendToCluster(repoName, localWorkDir)
 	if err == true {
 		fmt.Println("Failed to upload repo to ", dst)
 		return true
@@ -201,64 +171,46 @@ func (tcloudcli *TcloudCli) UploadRepo(localWorkDir string) bool {
 	fmt.Println("Successfully upload repo to ", dst)
 	return false
 }
-
 func (tcloudcli *TcloudCli) CondaCreate(repoName string, envName string) bool {
-	// bash_command := "ssh TACC1 /home/ubuntu/miniconda3/bin/conda env create -f /home/ubuntu/tcloud_job/configurations/conda.yaml"
-	// cmd := exec.Command("ssh", "-i", tcloudcli.userConfig.authFile, "ubuntu@18.162.45.250", bash_command)
-	// out, err := cmd.CombinedOutput()
-	// if err != nil {
-	// 	log.Fatalf("cmd.Run() failed with %s\n", err)
-	// 	return true
-	// }
-	// fmt.Printf("conda create on TACC1 out:\n%s\n", string(out))
-	// bash_command = "ssh TACC2 /home/ubuntu/miniconda3/bin/conda env create -f /home/ubuntu/tcloud_job/configurations/conda.yaml"
-	// cmd = exec.Command("ssh", "-i", tcloudcli.userConfig.authFile, "ubuntu@18.162.45.250", bash_command)
-	// out, err = cmd.CombinedOutput()
-	// if err != nil {
-	// 	log.Fatalf("cmd.Run() failed with %s\n", err)
-	// 	return true
-	// }
-	// fmt.Printf("conda create on TACC2 out:\n%s\n", string(out))
-	// return false
-
 	homeDir := fmt.Sprintf("/home/%s", tcloudcli.userConfig.UserName)
 	condaBin := fmt.Sprintf("%s/miniconda3/bin/conda", homeDir)
 	condaYaml := fmt.Sprintf("%s/%s/configurations/conda.yaml", homeDir, repoName)
 	cmd := fmt.Sprintf("%s %s env create -f %s\n", tcloudcli.prefix, condaBin, condaYaml)
-	if err := tcloudcli.tunnel.sess.Run(cmd); err != nil {
+	if err := tcloudcli.RemoteExecCmd(cmd); err == true {
 		fmt.Println("Failed to run cmd in CondaCreate ", err)
 		return true
 	}
-	write(tcloudcli.tunnel.in, cmd)
-	fmt.Println("Environment %s created.", envName)
+
+	fmt.Println("Environment \"", envName, "\" created.")
 	return false
 }
 func (tcloudcli *TcloudCli) CondaRemove(envName string) bool {
-	// bash_command := "ssh TACC1 /home/ubuntu/miniconda3/bin/conda remove -n " + name + " --all -y"
-	// cmd := exec.Command("ssh", "-i", tcloudcli.userConfig.authFile, "ubuntu@18.162.45.250", bash_command)
-	// out, err := cmd.CombinedOutput()
-	// if err != nil {
-	// 	log.Fatalf("cmd.Run() failed with %s\n", err)
-	// 	return true
-	// }
-	// fmt.Printf("conda remove on TACC1 out:\n%s\n", string(out))
-	// bash_command = "ssh TACC2 /home/ubuntu/miniconda3/bin/conda remove -n " + name + " --all -y"
-	// cmd = exec.Command("ssh", "-i", tcloudcli.userConfig.authFile, "ubuntu@18.162.45.250", bash_command)
-	// out, err = cmd.CombinedOutput()
-	// if err != nil {
-	// 	log.Fatalf("cmd.Run() failed with %s\n", err)
-	// 	return true
-	// }
-	// fmt.Printf("conda remove on TACC2 out:\n%s\n", string(out))
-	// return false
-
 	homeDir := fmt.Sprintf("/home/%s", tcloudcli.userConfig.UserName)
 	condaBin := fmt.Sprintf("%s/miniconda3/bin/conda", homeDir)
 	cmd := fmt.Sprintf("%s %s remove -n %s --all -y", tcloudcli.prefix, condaBin, envName)
-	if err := tcloudcli.sess.Run(cmd); err != nil {
+	if err := tcloudcli.RemoteExecCmd(cmd); err == true {
 		fmt.Println("Failed to run cmd in CondaRemove ", err)
 		return true
 	}
 	fmt.Println("Previous environment \"", envName, "\" removed.")
+	return false
+}
+
+func (tcloudcli *TcloudCli) XSubmit(args ...string) bool {
+	repoName := ""
+	if len(args) < 1 {
+		localWorkDir, _ := filepath.Abs(".")
+		dirlist := strings.Split(localWorkDir, "/")
+		repoName = dirlist[len(dirlist)-1]
+	} else {
+		repoName = args[0]
+	}
+	homeDir := fmt.Sprintf("/home/%s/%s", tcloudcli.userConfig.UserName, repoName)
+	cmd := fmt.Sprintf("%s sbatch %s/configurations/run.slurm", tcloudcli.prefix, homeDir)
+	if err := tcloudcli.RemoteExecCmd(cmd); err == true {
+		fmt.Println("Failed to run cmd in tcloud submit: ", err)
+		return true
+	}
+	fmt.Println("Job", repoName, "submitted.")
 	return false
 }
