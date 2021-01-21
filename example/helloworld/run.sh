913983e5a218e5786ac70062be3aa7a4f8312402
xcwan <523067817@qq.com>
2021-01-21 17:18:38 +0800

    xcompile.sh add setup.sh

--- example/helloworld/run.sh
+++ example/helloworld/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /mnt/sharefs/home/user6/.Miniconda3/etc/profile.d/conda.sh
-conda activate hello-be682e6a2d1fbd7357a70dc84651e9fd
+conda activate be682e6a2d1fbd7357a70dc84651e9fd
 
 python ${TACC_WORKDIR}/main.py \
--- xcompile.sh
+++ xcompile.sh
@@ -16,9 +16,25 @@ mkdir -p ${QUICKSTART_PATH}/bin/macos-amd64-v${VERSION}
 
 cd cli
 
+cat <<EOF >>setup.sh
+#!/bin/bash
+
+DIR=\$PWD
+
+mkdir -p \$HOME/.tcloud
+
+if [ ! -f \$BASH ]; then
+    touch \$BASH
+fi
+
+echo "Remember to execute the following command:"
+echo "export PATH=\$DIR:\$PATH"
+EOF
+
 # Linux
 make -f ${LINUX_MAKEFILE} clean && make -f ${LINUX_MAKEFILE} install
 cp bin/tcloud ${QUICKSTART_PATH}/bin/linux-amd64-v${VERSION}
+cp setup.sh ${QUICKSTART_PATH}/bin/linux-amd64-v${VERSION}
 
 # Windows
 make -f ${WINDOWS_MAKEFILE} clean && make -f ${WINDOWS_MAKEFILE} install
@@ -27,6 +43,9 @@ cp bin/tcloud ${QUICKSTART_PATH}/bin/windows-amd64-v${VERSION}
 # Mac
 make -f ${MAC_MAKEFILE} clean && make -f ${MAC_MAKEFILE} install
 cp bin/tcloud ${QUICKSTART_PATH}/bin/macos-amd64-v${VERSION}
+cp setup.sh ${QUICKSTART_PATH}/bin/macos-amd64-v${VERSION}
+
+rm setup.sh
 
 cd ${QUICKSTART_PATH}/bin
 zip -r linux-amd64-v${VERSION}.zip linux-amd64-v${VERSION}
